11c51829a416edc59f367d880f741607fb64e2dd
Sebastian Giraldo <sebastiangg990407@gmail.com>
2025-01-09 18:28:46 -0500

    first_commit

---
+++ src/Pages/App/index.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-red-100'>
+        <p>Hola mundo</p>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <div className="bg-blue-600">
+            <p>Home Page</p>
+        </div>
+    );
+}
+
+export {Home};
