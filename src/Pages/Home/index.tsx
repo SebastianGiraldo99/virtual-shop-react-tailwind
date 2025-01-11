856b4f23731d45a47304d85d5c4b6fa0f24efd7b
Sebastian Giraldo <sebastiangg990407@gmail.com>
2025-01-11 15:25:44 -0500

    creacion de la card usando tailwind

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,15 @@
+import { Card } from "../../Components/Card";
 import { Layout } from "../../Components/Layout";
+
+const objet1 = {id: 1, name: 'Name'};
 
 function Home(){
     return (
         <Layout>
             Home Page
+            <Card>
+                {objet1.name}
+            </Card>
         </Layout>
     );
 }
