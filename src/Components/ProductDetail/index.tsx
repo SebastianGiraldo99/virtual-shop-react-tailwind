540d15b9a9947c12483024a267ad711ce4bd18a2
Sebastian Giraldo <sebastiangg990407@gmail.com>
2025-01-14 10:56:46 -0500

    creacion de factory para instanciar interfaces de producto vacias, ajustes: #componente card para abrir el componente productDetail, #ajuste para cerrar el componente productDetail #vista de informacion en el componete productDetail #correccion y tipado del contexto.

--- src/Components/ProductDetail/index.tsx
+++ src/Components/ProductDetail/index.tsx
@@ -1,13 +1,30 @@
 import { XMarkIcon } from '@heroicons/react/16/solid';
 import './styles.css';
+import { useContext } from 'react';
+import { ShoppingCardContex } from '../../Context';
 const ProductDetail = () =>{
+    const context = useContext(ShoppingCardContex);
+        if(!context){
+            throw new Error('useContext debe ser usado dentro de ShoppingCardProvider');
+        }
+    const {showProductDetail, closeProductDetail, productToShow} = context;
+    console.log('PRODUCT TO SHOW : ',productToShow)
+
     return (
-        <aside className='product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg'>
+        <aside className={`${showProductDetail ? 'flex' : 'hidden'} product-detail  flex-col fixed bg-white right-0 border border-black rounded-lg`}>
             <div className='flex justify-between items-center p-6'> 
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <XMarkIcon className='w-6 h-6 text-black'></XMarkIcon>
-
+                <XMarkIcon onClick={()=>{closeProductDetail()}} className='w-6 h-6 text-black cursor-pointer'></XMarkIcon>
             </div>
+            <figure className=' px-6 '>
+                <img className='w-full h-full rounded-lg' 
+                src={productToShow.images?.[0] || ''} alt={productToShow.title || ''} />
+            </figure>
+            <p className='flex flex-col p-6'>
+                <span className='font-medium text-2xl mb-2'>${productToShow.price}</span>
+                <span className='font-medium text-md'>{productToShow.title}</span>
+                <span className='font-light text-sm'>{productToShow.description}</span>
+            </p>
         </aside>
     );
 }
